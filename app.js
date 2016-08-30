e9cac4dadf01a6d4640c3a38cab90c6347138a39
Pelayo Ramón <pelayo.ramon@clouddistrict.com>
2016-08-30 18:42:59 +0200

    added multipliers

--- app.js
+++ app.js
@@ -1,39 +1,64 @@
 let genetic = require('genetic')
 let _ = require('lodash')
 
-const solutionSize = 10
+const solutionSize = 11
+
 const checkValues = [
-  { mults: [0, 2, 4, 1, 0, 3, 3, 2, 5, 6], result: 15000},
-  { mults: [1, 6, 5, 2, 1, 1, 2, 5, 7, 6],result: 20000},
-  { mults: [1, 6, 5, 2, 2, 1, 2, 5, 7, 6],result: 21000},
-  { mults: [1, 6, 5, 2, 2, 3, 2, 5, 7, 6],result: 25000},
-  { mults: [1, 6, 5, 2, 2, 3, 2, 7, 7, 6],result: 30000},
-  { mults: [2, 5, 6, 3, 3, 2, 4, 9, 8, 5], result: 45000}
+  { mults: [0, 2, 1, 1.5, 5, 0, 0, 1, 1, 0, 1], result: 21000}, //COMISIONES
+  { mults: [1, 2, 0, 0, 4, 2, 1 ,2 ,0 ,0 ,0], result: 22000}, //reyournal
+  { mults: [3, 2, 0, 0, 3, 1, 1, 1, 1, 0, 0], result: 24000}, //PAGO
+  { mults: [3, 2, 0, 0, 0, 4, 4, 2, 0, 0, 1], result: 36000}, //NOFIRM
+  { mults: [2, 3, 0, 0, 4, 0, 1, 2, 4, 0, 1], result: 39000}, //GENTE
+  { mults: [1, 1 ,2, 3, 3, 1, 1, 1 ,2, 0, 0], result: 40000} //sofia
 ];
+
+const controlValue =  { mults: [0, 1, 1, 0, 5, 3, 4, 3, 0, 2 ,1],result: 28000} //WIDEVENTS
 
 const getRandomSolution = (callback) => {
   let solution = {
-    slopes: _.times(solutionSize, () => [  Math.random()*100, Math.random()*100, Math.random()*1000,  Math.random()*1000] )
+    slopes: _.times(solutionSize, () => [Math.random()*1000, Math.random()*500, Math.random()*40,  Math.random()*10] ),
+    gMultipliers: _.times(solutionSize, () => Math.random()*0.2 + 1),
   };
+
   callback(solution)
 }
 
 
 const getSolutionValue = (value, solution) => {
-  return Math.floor(_.reduce(
+  let baseValue = Math.floor(_.reduce(
     value.mults,
     function(res, val, ix){
-      res += solution[ix][0]*Math.pow(val,3) + solution[ix][1]*Math.pow(val,2) + solution[ix][2]*val + solution[ix][3];
+      res += _.reduce(
+        solution.slopes[ix],
+        (acum, solIx, pow) => {
+          acum += solIx*Math.pow(val, pow);
+          return acum;
+        },
+        0
+      );
       return res;
     },
-    -1 * value.result
+    0
   ));
+
+  let multValue = _.reduce(
+    value.mults,
+    function(res,val, ix){
+      if(val > 0)
+        res *= (solution.gMultipliers[ix]*val);
+
+      return res;
+    },
+    baseValue
+  );
+
+  return multValue - value.result;
 }
 
 const percentDeviation = (value, solution) => {
   const total = getSolutionValue(value, solution)
-  const res = 1 - (Math.abs(total) / value.result);
-  return res > 0 ? res : 0;
+  const res = 1 - (total/value.result);
+  return res;
 }
 
 
@@ -41,7 +66,7 @@ const fitness = (solution, callback) => {
   const total = _.reduce(
     checkValues,
     function(tot, value){
-      return tot + percentDeviation(value, solution.slopes);
+      return tot + percentDeviation(value, solution);
     },
     0
   );
@@ -50,59 +75,81 @@ const fitness = (solution, callback) => {
   callback(res);
 }
 
-const mutate = (solution, callback) => {
+const mutate = (solution, cb) => {
   let mutated = solution.slopes.slice();
+  let mutatedMults = solution.gMultipliers.slice();
 
   const randPos = _.random(0, mutated.length - 1);
   mutated[randPos] = _.map(
-    mutated[randPos], pos => {
-      return _.random(-2*pos, pos*10)
-    }
+    mutated[randPos],
+    pos => _.random(pos - pos/10, pos + pos/10)
   );
 
+  mutatedMults = _.map(
+    solution.gMultipliers,
+    val =>  _.random(val - val/10, val + val/10)
+  );
 
-  callback(solution);
+  solution.slopes = mutated;
+  solution.gMultipliers = mutatedMults;
+
+  cb(solution);
 }
 
-
 const crossover = (s1, s2, cb) => {
-  const parents = [s1.slopes,s2.slopes];
-  cb({
+  const parentsSlopes = [s1.slopes,s2.slopes];
+  const parentsMulipliers = [s1.gMultipliers,s2.gMultipliers];
+
+  const newSol = {
     slopes: _.times(
       solutionSize,
       function(ix){
-        return parents[_.random(0,parents.length - 1)][ix]
+        return parentsSlopes[_.random(0,parentsSlopes.length - 1)][ix]
       }
-    )
-  });
+    ),
+
+    gMultipliers:_.times(
+      solutionSize,
+      function(ix){
+        return parentsMulipliers[_.random(0,parentsMulipliers.length - 1)][ix]
+      }
+    ),
+
+  };
+
+  cb(newSol);
 }
 
-
-const stopCriteria = function(){
-  return this.generation == 1000 || (this.statistics && this.statistics.maxScore > 0.95);
+const getStopCriteria = () => {
+  return function(){
+    return this.generation == 2000 || (this.statistics && this.statistics.maxScore > 0.90);
+  }
 }
 
-let options = {
+const options = {
   getRandomSolution,
   popSize: 1000,
-  stopCriteria,
+  stopCriteria: getStopCriteria(),
   fitness,
-  mutateProbability: 0.3,
+  mutateProbability: 0.2,
   mutate,
-  crossoverProbability: 0.4,
+  crossoverProbability: 0.3,
   crossover
 }
 
 
-let task = new genetic.Task(options)
+const task = new genetic.Task(options)
 
 task.on('statistics', function (statistics) {
   console.log('Score',statistics.maxScore)
 })
 
 task.run( function(stats){
-  console.log('Results', stats.maxScore, stats.max.slopes)
+  console.log('Results', stats.maxScore, stats.max)
+
   _.each(checkValues, function(value){
-    console.log(value.result, getSolutionValue(value, stats.max.slopes));
+    console.log(value.result, getSolutionValue(value, stats.max));
   });
+
+  console.log("Control value",controlValue.result, getSolutionValue(controlValue, stats.max));
 });
